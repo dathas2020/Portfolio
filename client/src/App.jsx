f085e3544bb14c49403f9e5cba6e4ab88c04c4ae
dathas2020 <dathas2020@gmail.com>
2026-07-18 11:47:04 +0530

    Structure
    Folder structure created.

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
+import Research from "./pages/Research/Research";
+import Journey from "./pages/Journey/Journey";
+import Resume from "./pages/Resume/Resume";
+import Contact from "./pages/Contact/Contact";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/research" element={<Research />} />
+                    <Route path="/journey" element={<Journey />} />
+                    <Route path="/resume" element={<Resume />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
